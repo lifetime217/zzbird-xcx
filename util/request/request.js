e12f7ca4cd04935b3bbdcab6d000ad10628030b8
WSL <18523559538@163.com>
2019-02-27 18:01:27 +0800

    移除文件 Signed-off-by: WSL <18523559538@163.com>

--- util/request/request.js
+++ util/request/request.js
@@ -19,27 +19,6 @@ function httpPost(url, params) {
 }
 
 /**
- * 封装post请求
- */
-function httpPostJson(url, params) {
-  var promise = new Promise((resolve, reject) => {
-    //网络请求
-    wx.request({
-      url: url,
-      data: params,
-      method: 'POST',
-      header: {
-        'dataType':'json',
-        'content-type': 'application/json'
-      },
-      success: (res) => resolve(res),
-      fail: (res) => reject(res)
-    })
-  });
-  return promise;
-}
-
-/**
  * 封装get请求
  */
 function httpGet(url, params) {
@@ -56,28 +35,7 @@ function httpGet(url, params) {
   return promise;
 }
 
-/**
- * 调用自定义提示框
- */
-function toastShow(that, str, icon) {
-  that.setData({
-    isShow: true,
-    txt: str,
-    iconClass: icon
-  });
-  setTimeout(function() {
-    that.setData({
-      isShow: false
-    });
-  }, 1500);
-}
-
-
-
-
 module.exports = {
   httpPost: httpPost,
-  httpGet: httpGet,
-  toastShow: toastShow,
-  httpPostJson: httpPostJson
+  httpGet: httpGet
 }
