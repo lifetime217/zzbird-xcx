1a3ea864e31f59f18f3bf623087ebee28e02119b
WSL <18523559538@163.com>
2019-03-04 09:21:38 +0800

    修改首页的数据 Signed-off-by: WSL <18523559538@163.com>

--- pages/init/companyDetail/companyDetail.js
+++ pages/init/companyDetail/companyDetail.js
@@ -1,66 +1,127 @@
 // pages/init/companyDetail/companyDetail.js
+var http = require('../../../util/request/request.js');
+var app = getApp();
+var domainUrl = app.globalData.domainUrl;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    bannerList: [], //头部图片
+    companyDetail: {}, //公司详情
+    teaList: [], //教师列表
+    industry: [], //标签集合
+  
+  },
+  
+  // 显示加载框
+  showLoad: function() {
+    wx.showLoading({
+      title: '加载中...',
+      mask: true
+    })
+  },
+  // 隐藏加载框
+  hideTime: function() {
+    setTimeout(function() {
+      wx.hideLoading();
+    }, 1000);
+  },
+  /**
+   * 查询公司详情
+   */
+  queryCompanyDetail: function(companyId) {
+    var that = this;
+    that.showLoad();
+    var params = {
+      "companyId": companyId
+    }
+    // var roleType = app.globalData.roleType;
+    http.httpPost(domainUrl + "/company/queryCompanyDetail", params).then((res) => {
+      var data = res.data.data;
+      console.log(res);
+      // 查询成功
+      if (res.data.statusCode == 200) {
+        that.setData({
+          industry: data.industry,
+          bannerList: data.bannerList,
+          companyDetail: data.companyDetail,
+          teaList: data.companyTeacher
+        });
+      } else {
+        wx.showModal({
+          content: res.data.msg,
+          showCancel: false,
+        })
+      }
+      that.hideTime();
+    }).catch((errMsg) => {
+      console.log(errMsg);
+      wx.showModal({
+        content: '网络异常',
+        showCancel: false,
+      })
+      that.hideTime();
+    });
 
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad: function(options) {
+    // var companyId = options.companyId;
+    var companyId = "f06f6cccc4b34543bddb9568a86495e9";
+    this.queryCompanyDetail(companyId);
+   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
